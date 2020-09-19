4289b06e5f6e631ef657ebc7bfea14a4fa2855fc
sunyi <1181823629@qq.com>
2020-09-19 15:16:45 +0800

    工程项目初始化

---
+++ src/test/java/com/next/jiangzh/film/NextFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.jiangzh.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
